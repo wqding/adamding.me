58abddd1aad182f2d36991fc05245c486a9a5c74
Adam Ding <wqding@uwaterloo.ca>
2019-12-07 18:38:48 -0500

    added gulp and minified js and css

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,10 @@
 
 (function($) {
 
-    // Remove no-js class
     $('html').removeClass('no-js');
 
-    // Animate to section when nav is clicked
     $('header a').click(function(e) {
 
-        // Treat as normal link if no-scroll class
         if ($(this).hasClass('no-scroll')) return;
 
         e.preventDefault();
@@ -18,13 +15,11 @@
             scrollTop: scrollDistance + 'px'
         }, Math.abs(window.pageYOffset - $(heading).offset().top) / 1);
 
-        // Hide the menu once clicked if mobile
         if ($('header').hasClass('active')) {
             $('header, body').removeClass('active');
         }
     });
 
-    // Scroll to top
     $('#to-top').click(function() {
         $('html, body').animate({
             scrollTop: 0
@@ -39,17 +34,14 @@
         }, 500);
     });
 
-    // Open mobile menu
     $('#mobile-menu-open').click(function() {
         $('header, body').addClass('active');
     });
 
-    // Close mobile menu
     $('#mobile-menu-close').click(function() {
         $('header, body').removeClass('active');
     });
 
-    // Load additional projects
     $('#view-more-projects').click(function(e){
         e.preventDefault();
         $(this).fadeOut(300, function() {
