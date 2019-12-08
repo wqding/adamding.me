e42acce94f9c7a1297564c0f4f7a5d16cb55ee1a
Adam Ding <wqding@uwaterloo.ca>
2019-12-08 01:47:20 -0500

    remove js function

--- gulpfile.js
+++ gulpfile.js
@@ -6,22 +6,22 @@ var wait = require('gulp-wait');
 var rename = require('gulp-rename');
 var autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('scripts', function() {
-    return gulp.src('js/scripts.js')
-        .pipe(plumber(plumber({
-            errorHandler: function (err) {
-                console.log(err);
-                this.emit('end');
-            }
-        })))
-        .pipe(uglify({
-            output: {
-                comments: '/^!/'
-            }
-        }))
-        .pipe(rename({extname: '.min.js'}))
-        .pipe(gulp.dest('js'));
-});
+// gulp.task('scripts', function() {
+//     return gulp.src('js/scripts.js')
+//         .pipe(plumber(plumber({
+//             errorHandler: function (err) {
+//                 console.log(err);
+//                 this.emit('end');
+//             }
+//         })))
+//         .pipe(uglify({
+//             output: {
+//                 comments: '/^!/'
+//             }
+//         }))
+//         .pipe(rename({extname: '.min.js'}))
+//         .pipe(gulp.dest('js'));
+// });
 
 gulp.task('styles', function () {
     return gulp.src('./scss/styles.scss')
